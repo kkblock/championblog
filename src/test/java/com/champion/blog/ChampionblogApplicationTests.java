30dd5751e06172135d1a6ab885c7138a6360c2a6
kkblock <kuangkai_jy@163.com>
2018-08-16 14:23:38 +0800

    init

--- src/test/java/com/champion/blog/ChampionblogApplicationTests.java
+++ src/test/java/com/champion/blog/ChampionblogApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ChampionApplicationTests {
+public class ChampionblogApplicationTests {
 
     @Test
     public void contextLoads() {
