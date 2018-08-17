451001f77575ee8ffcbd1e5402dbf1f9b9d36c1b
kkblock <kuangkai_jy@163.com>
2018-08-17 09:21:29 +0800

    update

--- src/main/java/com/champion/blog/web/CityController.java
+++ src/main/java/com/champion/blog/web/CityController.java
@@ -1,7 +1,6 @@
 package com.champion.blog.web;
 
 import com.champion.blog.model.City;
-import com.champion.blog.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,9 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping(value = "/city")
 public class CityController extends BaseController {
 
-    @Autowired
-    private CityService cityService;
-
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public City findOneCity(@PathVariable("id") Long id, HttpServletRequest request) {
         City city = new City();
@@ -25,15 +21,6 @@ public class CityController extends BaseController {
         city.setCityName("沧州");
         city.setDescription("老家");
         return city;
-    }
-
-    @RequestMapping(value = "/testMongoTemplate",method = RequestMethod.GET)
-    public void testMongoTemplate(){
-        long j=0L;
-        Page<City> paginationQuery = cityService.paginationQuery(1);
-        for(City u:paginationQuery) {
-            System.out.println(u.getCityName());
-        }
     }
 
 }
