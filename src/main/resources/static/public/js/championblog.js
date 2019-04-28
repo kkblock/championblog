48d2a5a59be4d83be8e6e0e79a165cb9d28f89d1
kuangkai <kuangkai_jy@163.com>
2019-04-28 17:35:45 +0800

    update

--- src/main/resources/static/public/js/championblog.js
+++ src/main/resources/static/public/js/championblog.js
@@ -1,3 +1,34 @@
+//绑定鼠标事件
+$(document).on('mousewheel DOMMouseScroll', onMouseScroll);
+
+//鼠标滚动
+function onMouseScroll(e){
+    var wheel = e.originalEvent.wheelDelta || -e.originalEvent.detail;
+    var delta = Math.max(-1, Math.min(1, wheel) );
+    if(delta<0){//向下滚动
+        // $('div.main.menu').fadeOut();
+    }else{//向上滚动
+        // $('div.main.menu').fadeIn();
+    }
+}
+
+//显示weibo qq wechat 二维码函数
+function onlineshowmodal(param) {
+    switch (param) {
+        case 'weibo':
+            $('.weibopicmodal').modal('show');
+            break;
+        case 'wechat':
+            $('.wechatpicmodal').modal('show');
+            break;
+        case 'qq':
+            $('.qqpicmodal').modal('show');
+            break;
+        default:
+            console.error('modal not found');
+    }
+}
+
 function getContextPath() {
     var pathName = document.location.pathname;
     var index = pathName.substr(1).indexOf("/");
